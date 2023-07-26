ce82d8b656faf6780eee6c32e9ecc9829c789254
smkim <ksm0057@naver.com>
2023-07-26 20:04:02 +0900

    arrange UI by tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,32 @@
+import { Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./layout/Footer";
+import Navbar from "./layout/Navbar";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+const Layout: React.FC = () => {
+  return (
+    <div className="flex flex-col h-screen justify-between">
+      <Navbar />
+      <main className="mb-auto w-10/12 mx-auto">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
   );
 };
 
--- frontend/src/layout/Footer/index.tsx
+++ frontend/src/layout/Footer/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { AiOutlineSmile } from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex items-center justify-center h-20 text-lg">
+      Footer. <AiOutlineSmile />
+    </div>
+  );
 };
 
 export default Footer;
