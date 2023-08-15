9e7c0476e8e4be4271fa8a773ec412a062adb8c8
smkim <ksm0057@naver.com>
2023-08-15 20:07:47 +0900

    cors, json, middleware, mongoose connect, dotenv

--- backend/build/src/index.js
+++ backend/build/src/index.js
@@ -4,11 +4,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+//config
+dotenv_1.default.config();
 const path = require("path");
 const app = (0, express_1.default)();
 const port = 4000;
+//cors middleware
+app.use((0, cors_1.default)());
+// json parsing middleware
+app.use(express_1.default.json());
+// process.env.MONGO_URI! or process.env.MONGO_URI as string
+mongoose_1.default
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error(err));
 app.get("/", (req, res) => {
     res.send("Hello world!");
+});
+app.post("/", (req, res) => {
+    res.json(req.body);
 });
 app.use("/uploads", express_1.default.static(path.join(__dirname, "../../uploads")));
 app.listen(port, () => {
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,32 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+//config
+dotenv.config();
 
 const path = require("path");
 const app: Express = express();
 const port = 4000;
 
+//cors middleware
+app.use(cors());
+// json parsing middleware
+app.use(express.json());
+
+// process.env.MONGO_URI! or process.env.MONGO_URI as string
+mongoose
+  .connect(process.env.MONGO_URI!)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error(err));
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello world!");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  res.json(req.body);
 });
 
 app.use("/uploads", express.static(path.join(__dirname, "../../uploads")));
